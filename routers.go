ec95d95570bc1cd53455d7d3ed8b893db8d61576
雷永鑫 <yongxin@iscas.ac.cn>
2021-09-30 17:23:24 +0800

    add: supports s3 driver

--- routers.go
+++ routers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/a523/gin-mock-s3/config"
+	"github.com/a523/gin-mock-s3/s3"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,7 +17,11 @@ func setupRouter() *gin.Engine {
 		router.DELETE("/:bucket/:object/*objectSuffix", deleteFile)
 		router.GET("/:bucket/:object/*objectSuffix", getFile)
 	} else if config.CFG.Driver == "s3" {
-		router.GET("/:bucket/", getBucket)
+		router.GET("/:bucket/", s3.ListObjects)
+		router.HEAD("/:bucket/", s3.HeadBucket)
+		router.POST("/:bucket/", s3.DeleteObjects)
+		router.PUT("/:bucket/:object/*objectSuffix", s3.UploadObject)
+		router.GET("/:bucket/:object/*objectSuffix", s3.GetObject)
 	} else {
 		panic("Not supported the driver " + config.CFG.Driver)
 	}
--- s3/handler.go
+++ s3/handler.go
@@ -1,10 +1,18 @@
 package s3
 
 import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
 	"github.com/a523/gin-mock-s3/config"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"path"
 )
 
 func InitClient() *s3.Client {
@@ -34,4 +42,158 @@ func InitClient() *s3.Client {
 	})
 
 	return client
+}
+
+func ListObjects(c *gin.Context) {
+	_, queryLocation := c.GetQuery("location")
+	bucket := c.Param("bucket")
+	s3Client := InitClient()
+	if queryLocation {
+		lInput := s3.GetBucketLocationInput{Bucket: &bucket}
+		out, err := s3Client.GetBucketLocation(context.TODO(), &lInput)
+		if err != nil {
+			var re *awshttp.ResponseError
+			if errors.As(err, &re) {
+				c.XML(re.HTTPStatusCode(), err.Error())
+			} else {
+				c.XML(http.StatusInternalServerError, err.Error())
+			}
+
+		} else {
+			c.XML(http.StatusOK, &out)
+		}
+		return
+	}
+	listObjectsInput := s3.ListObjectsInput{Bucket: &bucket}
+	err := c.BindQuery(listObjectsInput)
+	if err != nil {
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	out, err := s3Client.ListObjects(context.TODO(), &listObjectsInput)
+	if err != nil {
+		var re *awshttp.ResponseError
+		if errors.As(err, &re) {
+			c.XML(re.HTTPStatusCode(), err.Error())
+			return
+		}
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.XML(http.StatusOK, &out)
+}
+
+func HeadBucket(c *gin.Context) {
+	bucket := c.Param("bucket")
+	headBucketInput := s3.HeadBucketInput{Bucket: &bucket}
+	err := c.BindQuery(headBucketInput)
+	if err != nil {
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	s3Client := InitClient()
+
+	out, err := s3Client.HeadBucket(context.TODO(), &headBucketInput)
+	if err != nil {
+		var re *awshttp.ResponseError
+		if errors.As(err, &re) {
+			c.XML(re.HTTPStatusCode(), err.Error())
+			return
+		}
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.XML(http.StatusOK, &out)
+}
+
+func DeleteObjects(c *gin.Context) {
+	bucket := c.Param("bucket")
+	_, del := c.GetQuery("delete")
+
+	if del {
+		deleteObjectsInput := s3.DeleteObjectsInput{Bucket: &bucket}
+		err := c.BindXML(deleteObjectsInput)
+		if err != nil {
+			c.XML(http.StatusBadRequest, err.Error())
+			return
+		}
+		err = c.BindQuery(deleteObjectsInput)
+
+		s3Client := InitClient()
+
+		out, err := s3Client.DeleteObjects(context.TODO(), &deleteObjectsInput)
+		if err != nil {
+			var re *awshttp.ResponseError
+			if errors.As(err, &re) {
+				c.XML(re.HTTPStatusCode(), err.Error())
+				return
+			}
+			c.XML(http.StatusBadRequest, err.Error())
+			return
+		}
+		c.XML(http.StatusOK, &out)
+		return
+	} else {
+		c.XML(http.StatusInternalServerError, "not supported")
+		return
+	}
+}
+
+func UploadObject(c *gin.Context) {
+	bucket := c.Param("bucket")
+	object := c.Param("object")
+	objectSuffix := c.Param("objectSuffix")
+	key := path.Join(object, objectSuffix)
+
+	data, err := c.GetRawData()
+	if err != nil {
+		fmt.Printf("Error uploading : %s", err)
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	putObjectInput := s3.PutObjectInput{Bucket: &bucket, Key: &key, Body: bytes.NewReader(data)}
+	err = c.BindQuery(putObjectInput)
+	if err != nil {
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	s3Client := InitClient()
+
+	out, err := s3Client.PutObject(context.TODO(), &putObjectInput)
+	if err != nil {
+		var re *awshttp.ResponseError
+		if errors.As(err, &re) {
+			c.XML(re.HTTPStatusCode(), err.Error())
+			return
+		}
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.XML(http.StatusOK, &out)
+}
+
+func GetObject(c *gin.Context) {
+	bucket := c.Param("bucket")
+	object := c.Param("object")
+	objectSuffix := c.Param("objectSuffix")
+	key := path.Join(object, objectSuffix)
+	getObjectInput := s3.GetObjectInput{Bucket: &bucket, Key: &key}
+	s3Client := InitClient()
+
+	out, err := s3Client.GetObject(context.TODO(), &getObjectInput)
+	if err != nil {
+		var re *awshttp.ResponseError
+		if errors.As(err, &re) {
+			c.XML(re.HTTPStatusCode(), err.Error())
+			return
+		}
+		c.XML(http.StatusBadRequest, err.Error())
+		return
+	}
+	extraHeaders := map[string]string{
+		"Last-Modified": out.LastModified.UTC().Format(http.TimeFormat),
+		"ETag":          *out.ETag,
+	}
+	c.DataFromReader(200, out.ContentLength, *out.ContentType, out.Body, extraHeaders)
 }
